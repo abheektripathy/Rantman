1f1fc0f156784a3a08c786de4635c03e25920650
Abheek Tripathy <abheek.tripathy@gmail.com>
2022-10-29 05:04:39 +0530

    frontendfixes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,23 +11,7 @@ import { promises } from "fs"
 export const App = () => {
    
 
-  const [notes, setNotes] = useState([])
 
-  useEffect(()=> {
-    getNotes()
-
-  },[])
-
-  let getNotes = async() => {
-    let response = await fetch('http://127.0.0.1:8000/api/notes')
-    //taaku usko fetch karne ka time de tabhi await use kiya 
-    let data = await response.json()
-    setNotes(data)
-  
-
-    
-    
-  }
 
 
 
--- frontend/src/pages/NotesListPage.tsx
+++ frontend/src/pages/NotesListPage.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const NotesListPage = () => {
+
+    const [notes, setNotes] = useState([])
+
+    useEffect(()=> {
+      getNotes()
+  
+    },[])
+  
+    let getNotes = async() => {
+      let response = await fetch('http://127.0.0.1:8000/api/notes')
+      //taaku usko fetch karne ka time de tabhi await use kiya 
+      let data = await response.json()
+      setNotes(data)
+      
+    
+  
+      
+      
+    }
   return (
     <div>
+        <div className='notes-list'></div>
+        {notes.map((note,index)=>(
+            <h3></h3>
+         
+        ))}
       
     </div>
   )
