b3d6fef1eb465879a8a077930266b17999e40373
Jayant <39442192+RoguedBear@users.noreply.github.com>
2023-04-24 13:28:20 +0000

    fix local bind to global bind. ~~ end of rant. sincerely, rant man

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py makemigrations
 python manage.py migrate --no-input
 # python manage.py collectstatic --no-input
 
-gunicorn rantman.wsgi:application  --bind 127.0.0.1:8000
+gunicorn rantman.wsgi:application  --bind 0.0.0.0:8000
