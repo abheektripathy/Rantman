1401061c712066fdabef6727fa4bf4d9c632b6bf
Abheek Tripathy <abheek.tripathy@gmail.com>
2023-04-24 05:52:17 +0530

    added up a 404

--- frontend/pages/notes/index.tsx
+++ frontend/pages/notes/index.tsx
@@ -45,53 +45,71 @@ export default function NotesPage() {
       >
         /api/notes/all
       </h1>
-      <section className="bg-inherit text-white -mt-4">
-        <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
-          <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-            {mockData ? (
-              mockData.map((note) => (
-                <button
-                  key={note.id}
-                  className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
-                  onClick={() => {
-                    // handle clicking on the note
-                  }}
-                >
-                  <Link href={`/notes/${note.id}`}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-10 w-10 text-blue-500"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path d="M12 14l9-5-9-5-9 5 9 5z" />
-                      <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                      />
-                    </svg>
+      {mockData ? (
+        <section className="bg-inherit text-white -mt-4">
+          <div className="mx-auto max-w-screen-xl px-4 py-8 sm:py-12 sm:px-6 lg:py-16 lg:px-8">
+            <div className="mt-8 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
+              {mockData ? (
+                mockData.map((note) => (
+                  <button
+                    key={note.id}
+                    className="block rounded-xl border border-gray-800 p-8 shadow-xl transition hover:border-pink-500/10 hover:shadow-pink-500/10"
+                    onClick={() => {
+                      // handle clicking on the note
+                    }}
+                  >
+                    <Link href={`/notes/${note.id}`}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-10 w-10 text-blue-500"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                        <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                        />
+                      </svg>
 
-                    <h2 className="mt-4 text-xl font-bold text-white text-left">
-                      {note.id}
-                    </h2>
+                      <h2 className="mt-4 text-xl font-bold text-white text-left">
+                        {note.id}
+                      </h2>
 
-                    <p className="mt-1 text-sm text-gray-300 text-left">
-                      {note.body.split(" ").slice(0, 20).join(" ")}
-                      {note.body.split(" ").length > 20 ? "..." : ""}
-                    </p>
-                  </Link>
-                </button>
-              ))
-            ) : (
-              <p>Loading...</p>
-            )}
+                      <p className="mt-1 text-sm text-gray-300 text-left">
+                        {note.body.split(" ").slice(0, 20).join(" ")}
+                        {note.body.split(" ").length > 20 ? "..." : ""}
+                      </p>
+                    </Link>
+                  </button>
+                ))
+              ) : (
+                <p>Loading...</p>
+              )}
+            </div>
+          </div>
+        </section>
+      ) : (
+        <div className="grid h-screen px-4 bg-inherit place-content-center dark:bg-inherit pb-32">
+          <div className="text-center">
+            <h1 className="font-black text-gray-200 text-9xl dark:text-gray-700">
+              404
+            </h1>
+
+            <p className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-4xl">
+              Uh-oh!
+            </p>
+
+            <p className="mt-4 text-gray-500 dark:text-gray-400">
+              try creating some notes? :p
+            </p>
           </div>
         </div>
-      </section>
+      )}
     </>
   );
 }
